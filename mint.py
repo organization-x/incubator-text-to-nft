8a9eeb8e5d0fcd1aea45fd38f0fb12b882a1f8c8
Aditya Velusamy <adityavelusamy@gmail.com>
2022-09-25 18:02:57 -0700

    Update mint.py

--- mint.py
+++ mint.py
@@ -4,7 +4,7 @@ from eth_account import Account
 from dotenv import load_dotenv
 from web3 import Web3
 import os
-
+# variables to make implementing easier in future
 PRIVATE_KEY = "2d69642fc9b97c1db045b87b4b0f865251caa1c3737b052a08b395a6f0e62ce4"
 sdk = ThirdwebSDK.from_private_key(PRIVATE_KEY, "goerli")
 NFT_COLLECTION_ADDRESS = "0x78fA9B9C98240dd11F10a096846a5ba0cb51ab15"
@@ -22,7 +22,7 @@ nft_name4 = "House4"
 description4 = "An other more different house"
 image4 = "https://pbs.twimg.com/media/FY2ZpDJXEAAQo8c.jpg"
 properties = {}
-
+# code to mint the NFTs
 nft_collection.mint(NFTMetadataInput.from_json({ "name": nft_name1, "description": description1, "image": image1}))
 nft_collection.mint(NFTMetadataInput.from_json({ "name": nft_name2, "description": description2, "image": image2}))
 nft_collection.mint(NFTMetadataInput.from_json({ "name": nft_name3, "description": description3, "image": image3}))
