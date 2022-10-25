21e307aba5ee49f3b4ede3a9fdc4defcbd6b206f
unknown <071bct525prasanga@pcampus.edu.np>
2022-10-25 11:51:18 -0500

    fixed backend issue in node

--- App.js
+++ App.js
@@ -1,26 +1,40 @@
-const express = require('express');
-const http = require('http');
-const {PythonShell} =require('python-shell');
-const {middleware} =require('connect');
-const hostname = 'localhost';
-const app = express();
-const port = 4500;
+//Import express.js module and create its variable.
+const express=require('express');
+const app=express();
 
-const server = http.createServer((req, res) => {
-   // PythonShell.run('mint.py', null, function (err) {
-  //   if (err) throw err;
-  //   console.log('finished');
-  // });
-  console.log(`Prior to post`);
-  app.use(express.json());
-app.use(express.urlencoded());
-app.post('/post', function(req, res) {
-  console.log("works");
-  var name = req.body
-  console.log(name);
-  
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded()); // to support URL-encoded bodies
+
+//Import PythonShell module.
+const {PythonShell} =require('python-shell');
+
+app.post("/post", (req, res, next)=>{
+    console.log(" POST REQUEST");
+    console.log(req.body);
+
+    let options = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+          scriptPath: 'D:\\AICamp\\Fall-Incubator\\Tutorials\\node-server', //If you are having python_test.py script in same folder, then it's optional.
+        args: ['pass-the-promt-and-other-variables'] //An argument which can be accessed in the script using sys.argv[1]
+    };
+
+
+    PythonShell.run('my_script.py', options, function (err, result){
+          if (err) throw err;
+
+          console.log('result: ', result.toString());
+    });
+    // We will have to respond with the URL of the generated image.
+    res.redirect("/MVP")
 });
-});
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+
+//Creates the server on default port 8000 and can be accessed through localhost:8000
+const port= process.env.PORT || 8000;
+app.listen(port, ()=>console.log(`Server connected to ${port}`));
