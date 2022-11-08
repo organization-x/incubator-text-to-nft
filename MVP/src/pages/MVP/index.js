d0f7c8a198e4d1b4def45864f8f4e3e5aa8127d8
Aditya Velusamy <adityavelusamy@gmail.com>
2022-11-07 17:16:09 -0800

    Update index.js

--- MVP/src/pages/MVP/index.js
+++ MVP/src/pages/MVP/index.js
@@ -1,5 +1,33 @@
 import React from "react";
+import { useState } from 'react';
+import { ethers } from 'ethers';
 import { Column, Text, Img, Stack } from "components";
+const App = () => {
+
+  const homeWallet = "0xf69C8DA5188FB241017b68C367fd95f6CB62Bb92";
+  const [walletAddress, setWalletAddress] = useState("");
+  const [accountBalance, setAccountBalance] = useState("");
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  
+  async function requestAccount() {
+    console.log('requesting account...');
+
+    // checking if user has MetaMask extension
+    if(window.ethereum) {
+      console.log('Metamask detected');
+
+    //accessing wallet/account number
+      try {
+        const accounts = await window.ethereum.request({method: "eth_requestAccounts",});
+        setWalletAddress(accounts[0]);
+      } catch (error) {
+        console.log('Wallet not found...'); 
+      }
+    } else {
+      console.log('Metamask not detected');
+    }
+  }
+}
 const MVPPage = () => {
   return (
     <>
@@ -44,8 +72,19 @@ const MVPPage = () => {
               Connect
             </Text>
             </button>
+            {/* <button id="connect" type="submit"> 
+            <Stack className="absolute lg:h-[51px] xl:h-[58px] 2xl:h-[65px] 3xl:h-[78px] w-[100%]" style={{'top':'195%'}}>
+              <div className="absolute bg-bluegray_800 lg:h-[51px] xl:h-[58px] 2xl:h-[65px] 3xl:h-[78px] inset-x-[13%] w-[74%]"></div>
+              <div className="absolute bg-bluegray_801 lg:h-[51px] xl:h-[58px] 2xl:h-[65px] 3xl:h-[78px] right-[0] rounded-radius50 lg:w-[50px] xl:w-[57px] 2xl:w-[64px] 3xl:w-[77px]"></div>
+              <div className="absolute bg-bluegray_801 lg:h-[51px] xl:h-[58px] 2xl:h-[65px] 3xl:h-[78px] left-[0] rounded-radius50 lg:w-[50px] xl:w-[57px] 2xl:w-[64px] 3xl:w-[77px]" ></div>
+            </Stack>
+            <Text className="absolute font-normal h-[max-content] inset-[0] justify-center m-[auto] not-italic lg:text-[26px] xl:text-[30px] 2xl:text-[34px] 3xl:text-[41px] text-white_A700 w-[max-content]" style={{'top':'245%', 'font-size':"18px"}}>
+              Connect With MetaMask
+            </Text>
+            </button> */}
           </Stack>
           </Column>
+
       </Column>
       </form>
     </>
@@ -53,4 +92,3 @@ const MVPPage = () => {
 };
 
 export default MVPPage;
- {/* */}
