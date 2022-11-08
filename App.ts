25a5bf3223f35cf71b8bce7f1d2c371aa766dec2
Aditya Velusamy <adityavelusamy@gmail.com>
2022-11-07 17:41:57 -0800

    Forgot to commit TS

--- App.ts
+++ App.ts
@@ -1,7 +1,8 @@
 //Import express.js module and create its variable.
 const express=require('express');
 const app=express();
-import { ThirdwebSDK } from "@thirdweb-dev/sdk";
+import { BigNumber, ethers } from "ethers";
+import { ThirdwebSDK, TransactionResult } from "@thirdweb-dev/sdk";
 import { PythonShell } from 'python-shell';
 var bodyParser = require('body-parser')
 app.use( bodyParser.json() );       // to support JSON-encoded bodies
@@ -12,6 +13,8 @@ app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 app.use(express.json());       // to support JSON-encoded bodies
 app.use(express.urlencoded()); // to support URL-encoded bodies
 let promptforsys: any = "";
+const RPC_URL = "https://rpc.testnet.fantom.network/"
+let contractAddress: any;
 //Import PythonShell module.
 app.post("/Promptpost", (req :any, res :any, next :any)=>{
   
@@ -31,38 +34,52 @@ app.post("/Promptpost", (req :any, res :any, next :any)=>{
   //       if (err) throw err;
   // });
   // We will have to respond with the URL of the generated image.
-  res.redirect("/MVP")
+  res.redirect("/mint")
 });
+// make an async function
+async function main(address, privateKey) {
+  const sdk = new ThirdwebSDK(
+    new ethers.Wallet(   
+    privateKey,
+    ethers.getDefaultProvider(RPC_URL)
+    )
+);
+
+let collectionAddress:string;
+let mintTxnHash:TransactionResult;
+   contractAddress = await sdk.deployer.deployNFTCollection({
+    name: promptforsys,
+    primary_sale_recipient: address
+  });
+  console.log("Contract Address: ", contractAddress);
+}
 app.post("/Walletpost", (req :any, res :any, next :any)=>{
     console.log("POST REQUEST");
     // console.log(req.body);
     let addressforsys: any = req.body.address;
-    const RPC_URL = "https://rpc.testnet.fantom.network/"
     console.log(addressforsys);
-    const sdk = new ThirdwebSDK(RPC_URL);
-    const contractAddress = await sdk.deployer.deployNFTCollection({
-      name: "My Collection",
-      primary_sale_recipient: "your-address",
-    });
+    
   //   const contractsarray: any = sdk.getContractList(addressforsys);
   //  contractsarray.then(contractsarray => console.log(contractsarray));
 
 
-    // let keyforsys: any = req.body.key;
-    // console.log(keyforsys);
+    let keyforsys: any = req.body.key;
+    console.log(keyforsys);
+    main(addressforsys, keyforsys);
+    setTimeout(() => {
+      let options :any = {
+        mode: 'text',
+        pythonOptions: ['-u'], // get print results in real-time
+        // scriptPath: '/Users/adityav/Downloads/GitHub/incubator-text-to-nft/mint.py', //If you are having python_test.py script in same folder, then it's optional.
+        args: [contractAddress, promptforsys, keyforsys] //An argument which can be accessed in the script using sys.argv[1]
+    };
 
-    // let options :any = {
-    //     mode: 'text',
-    //     pythonOptions: ['-u'], // get print results in real-time
-    //     // scriptPath: '/Users/adityav/Downloads/GitHub/incubator-text-to-nft/mint.py', //If you are having python_test.py script in same folder, then it's optional.
-    //     args: [addressforsys, promptforsys, keyforsys] //An argument which can be accessed in the script using sys.argv[1]
-    // };
-
-    // PythonShell.run('NFTMint.py', options, function (err :any, result :any){
-    //       if (err) throw err;
-    // });
-    // We will have to respond with the URL of the generated image.
-    res.redirect("/MVP")
+    PythonShell.run('NFTMint.py', options, function (err :any, result :any){
+          if (err) throw err;
+    });
+    }, 10000)
+    
+    res.redirect("/mint")
 });
 
 //Creates the server on default port 8000 and can be accessed through localhost:8000
