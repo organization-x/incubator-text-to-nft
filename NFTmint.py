8e80f99ffd8597a399d052e78042f0ed1e2ceefd
unknown <071bct525prasanga@pcampus.edu.np>
2022-11-16 15:45:07 -0600

    repo cleanup

--- NFTmint.py
+++ NFTmint.py
@@ -1,4 +1,5 @@
 import getpass, os
+import sys
 from thirdweb import ThirdwebSDK
 from thirdweb.types.nft import NFTMetadataInput
 from eth_account import Account
@@ -12,26 +13,28 @@ from stability_sdk import client
 import stability_sdk.interfaces.gooseai.generation.generation_pb2 as generation
 import random
 import google.cloud.storage
-
+load_dotenv()
 def uploadFileToBucket(serviceAcctFile, bucket_name, blob_name, fileToUpload):
     storage_client = google.cloud.storage.Client.from_service_account_json(serviceAcctFile)
     bucket = storage_client.get_bucket(bucket_name)
     blob = bucket.blob(blob_name)
     blob.upload_from_filename(fileToUpload)
-
+RPC_URL = "https://rpc.testnet.fantom.network/"
+NFT_COLLECTION_ADDRESS = "00000000000000000000000000000000"
+ETH_WALLET_PRIVATE_KEY = "0000000000000000000000000000000000000000000000000000000000000000"
 DREAM_STUDIO_API_KEY = os.getenv("DREAM_STUDIO_API_KEY")
 URL_BEGINNING = "https://storage.googleapis.com/"
-ETH_WALLET_PRIVATE_KEY = os.getenv("ETH_WALLET_PRIVATE_KEY")
-NFT_COLLECTION_ADDRESS = os.getenv("NFT_COLLECTION_ADDRESS")
-sdk = ThirdwebSDK.from_private_key(ETH_WALLET_PRIVATE_KEY, "goerli")
+ETH_WALLET_PRIVATE_KEY = sys.argv[3]
+NFT_COLLECTION_ADDRESS = sys.argv[1]
+sdk = ThirdwebSDK.from_private_key(ETH_WALLET_PRIVATE_KEY, RPC_URL)
 nft_collection = sdk.get_nft_collection(NFT_COLLECTION_ADDRESS)
 SERVICE_ACCT_JSON_FILE = os.getenv("SERVICE_ACCT_JSON_FILE")
 BUCKET_NAME = os.getenv("BUCKET_NAME")
 
 stability_api = client.StabilityInference(key = DREAM_STUDIO_API_KEY, verbose=True,)
 
-prompt = input('What do you want Text to NFT to draw? ')
-numImgs = input("How many images would you like? ")
+prompt = sys.argv[2]
+numImgs = sys.argv[4]
 
 #generate object returned is a python generator
 for i in range(int(numImgs)):
@@ -59,5 +62,4 @@ for i in range(int(numImgs)):
     description = "n/a"
     
     # Mints image url to collection address
-    nft_collection.mint(NFTMetadataInput.from_json({ "name": imgName, "description": description, "image": imgUrlWithoutSpaces}))
-
+    nft_collection.mint(NFTMetadataInput.from_json({ "name": imgName, "description": prompt, "image": imgUrlWithoutSpaces}))
--- README.md
+++ README.md
@@ -1,29 +1,30 @@
-﻿# incubator-text-to-nft
+﻿﻿# incubator-text-to-nft
 
 <h3>Introduction:</h3> 
 
-Our Text to NFT product is one that allows users create a collection of NFTs using a text prompt given by the user. These NFTs can then be found on various collection sites such as Thirdweb and OpenSea. This product was created by the Text-to-NFT group during AI Camp's Team-Tomorrow Incubator program.
+Our Text to NFT product is one that allows users create a collection of NFTs using a text prompt given by the user. These NFTs can then be found on various collection sites such as Etherscan, OpenSea and Thirdweb. This product was created by the Text-to-NFT group during AI Camp's Team-Tomorrow Incubator program.
 
 <h3>How To Run Our Product:</h3> 
 
-**1. gitclone this reopsitory**
+**1. Clone this repo: `git clone <this-reopsitory>`**
 
-**2. pip install necessary modules and packages:**
+**2. Getting Techstack Ready:**
 
-
-    - Pip
     - Python
-    - All necessary packages for NFTmint.py 
-    - node
-    - npm
+    - Node
 
+**3. Setting up python modules: `pip install -r requirements.txt`**
+     
 
-**3. Add stability key:** Either create a variable in the NFTmint.py script or create an environment variable.
-
-**4. Commands for hosting:**
-   Now that we have installed all the necessary modules and packages, it is now time to host the website. The following are the commands to host:
-   - Hosting Frontend: cd <into the MVP directory → npm install → npm start
-   - Hosting Backend: npm install → node App.js
+**4. Required API Keys:** <br>
+    
+    - Stability API: Either create a variable in the NFTMint.py script or create an environment variable.
+    - Cloud Storage for Generated Images: Use GCP or other could credentials in NFTMint.py or create a seprate json file.  
+    
+**5. To run:**
+   Now that we have installed all the necessary modules and packages, it is now time to test the website. The following are the commands to run:
+   - Frontend: `cd <into the MVP directory>` → `npm install` → `npm start`
+   - Backend: `npm install` → `node App.js`
 
 
 <h3>Try Our Product:</h3>   
